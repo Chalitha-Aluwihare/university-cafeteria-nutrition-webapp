c906b52b304c806aa873ed545fffe66c048addea
Chalitha Aluwihare <147813661+Chalitha-Aluwihare@users.noreply.github.com>
2025-06-15 21:08:07 +0530

    Set up basic structure for Home and Meal Plan pages

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <>
+            <div className='items-center text-center justify-center bg-black text-white pt-2'>
+                <h1 className='text-[20px] font-semibold pt-2'>Popular Categories</h1>
+                <div className='mt-2'>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Main Meals</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Beverages</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Short Eats</h3>
+                </div>
+
+                <h1 className='text-[20px] font-semibold pt-2'>Get to Know Us</h1>
+                <div className='mt-2'>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>About Us</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Our Story</h3>
+                </div>
+
+                <h1 className='text-[20px] font-semibold pt-2'>Quick Links</h1>
+                <div className='mt-2'>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Home</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Offers & Discounts</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Account</h3>
+                </div>
+
+                <h1 className='text-[20px] font-semibold pt-2'>Customer Support</h1>
+                <div className='mt-2'>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Help Center</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Contact Us</h3>
+                </div>
+
+                <h1 className='text-[20px] font-semibold pt-2'>Powered By</h1>
+                <div className='mt-2 pb-4'>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Uva Wellassa University of Sri Lanka</h3>
+                    <h3 className='text-[15px] text-[#C9C9C9]'>Faculty of Technological Studies</h3>
+                    <h3 className='text-[13px] text-[#C9C9C9]'>Department of Information and Communication Technology</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
