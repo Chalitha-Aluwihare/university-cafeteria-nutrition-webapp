e49bc359bf9e2f84d5f9a4f7c37c4f057194c7c7
Chalitha Aluwihare <147813661+Chalitha-Aluwihare@users.noreply.github.com>
2025-07-28 19:33:20 +0530

    setup admin panel and completed all MainMeals page functions

---
+++ admin-panel/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/navBar.jsx';
+import MainMeals from './pages/mainMeals.jsx';
+
+function App() {
+  return (
+    <div>
+      < NavBar />
+      <MainMeals />
+    </div>
+  );
+}
+
+export default App;
+
+
