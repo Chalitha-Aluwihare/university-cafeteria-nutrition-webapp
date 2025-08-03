365c1e876b379ffb3601a023892edbc0cf059239
Chalitha Aluwihare <147813661+Chalitha-Aluwihare@users.noreply.github.com>
2025-08-03 14:41:16 +0530

    basic backend setup complete

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://new-user-01:user123@unieatscluster.dsrxvz8.mongodb.net/UniEats-WebApp").then(() => console.log("MongoDB connected successfully"));
+    }
